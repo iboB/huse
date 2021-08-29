7ad80d5df8b898919d7dbb7b521a9b0c5cb11f79
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-08-29 07:13:34 +0300

    Test improvements

--- test/t-json.cpp
+++ test/t-json.cpp
@@ -13,53 +13,91 @@
 
 TEST_SUITE_BEGIN("json");
 
-struct JsonOut {
+struct JsonSerializerPack {
     std::ostringstream sout;
-    huse::JsonSerializer s;
+    std::optional<huse::JsonSerializer> s;
 
-    JsonOut(bool pretty = false)
-        : s(sout, pretty)
-    {}
-
-    huse::JsonSerializer* operator->() {
-        return &s;
+    JsonSerializerPack(bool pretty)
+    {
+        s.emplace(sout, pretty);
     }
 
-    std::string str() {
+    std::string str()
+    {
+        s.reset();
         return sout.str();
+    }
+};
+
+struct JsonOutTester {
+    std::optional<JsonSerializerPack> pack;
+
+    huse::JsonSerializer& make(bool pretty = false) {
+        assert(!pack);
+        pack.emplace(pretty);
+        return *pack->s;
+    }
+
+    huse::JsonSerializer& compact() { return make(false); }
+    huse::JsonSerializer& pretty() { return make(true); }
+
+    std::string str()
+    {
+        std::string ret = pack->str();
+        pack.reset();
+        return ret;
     }
 };
 
 TEST_CASE("simple serialize")
 {
+    JsonOutTester j;
+
+    j.compact().val(5);
+    CHECK(j.str() == "5");
+
+    j.compact().val(nullptr);
+    CHECK(j.str() == "null");
+
     {
-        JsonOut j;
-        j->val(5);
-        CHECK(j.str() == "5");
-    }
-    {
-        JsonOut j;
-        j->val(nullptr);
-        CHECK(j.str() == "null");
-    }
-    {
-        JsonOut j;
+        auto obj = j.compact().obj();
         {
-            auto obj = j->obj();
-            {
-                auto ar = obj.ar("array");
-                for (int i = 1; i < 5; ++i) ar.val(i);
-            }
-            std::optional<int> nope;
-            std::optional<int> yup = -3;
-            obj.val("bool",true);
-            obj.val("bool",false);
-            obj.val("float",3.1f);
-            obj.val("int", yup);
-            obj.val("nope", nope);
-            obj.val("unsigned-long-long",900000000000000ULL);
-            obj.val("str", "b\n\\g\t\033sdf");
+            auto ar = obj.ar("array");
+            for (int i = 1; i < 5; ++i) ar.val(i);
         }
-        CHECK(j.str() == R"({"array":[1,2,3,4],"bool":true,"bool":false,"float":3.1,"int":-3,"unsigned-long-long":900000000000000,"str":"b\n\\g\t\u001bsdf"})");
+        std::optional<int> nope;
+        std::optional<int> yup = -3;
+        obj.val("bool",true);
+        obj.val("bool",false);
+        obj.val("float",3.1f);
+        obj.val("int", yup);
+        obj.val("nope", nope);
+        obj.val("unsigned-long-long",900000000000000ULL);
+        obj.val("str", "b\n\\g\t\033sdf");
     }
+    CHECK(j.str() == R"({"array":[1,2,3,4],"bool":true,"bool":false,"float":3.1,"int":-3,"unsigned-long-long":900000000000000,"str":"b\n\\g\t\u001bsdf"})");
+
+    j.compact().obj().obj("i1").obj("i2").obj("i3").val("deep", true);
+    CHECK(j.str() == R"({"i1":{"i2":{"i3":{"deep":true}}}})");
+
+    {
+        auto obj = j.pretty().obj();
+        {
+            obj.val("pretty", true);
+            auto ar = obj.ar("how_much");
+            ar.val("very");
+            ar.val("very");
+            ar.val("much");
+        }
+    }
+    CHECK(j.str() ==
+R"({
+  "pretty":true,
+  "how_much":[
+    "very",
+    "very",
+    "much"
+  ]
+})"
+    );
 }
