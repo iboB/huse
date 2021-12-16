568e1215ebe28efa2d866fde674ef9a8bc5d287a
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-16 09:15:27 +0200

    Explicit serializer ctor

--- code/huse/json/Serializer.hpp
+++ code/huse/json/Serializer.hpp
@@ -17,7 +17,7 @@ namespace huse::json
 class HUSE_API Serializer final : public BasicSerializer
 {
 public:
-    Serializer(std::ostream& out, bool pretty = false, Context ctx = {});
+    explicit Serializer(std::ostream& out, bool pretty = false, Context ctx = {});
     ~Serializer();
 
     Serializer(const Serializer&) = delete;
