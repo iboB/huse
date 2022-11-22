67245bab8c59f3cfb13e23a04926684050afe22c
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-11-22 13:05:36 +0200

    Fixed constant

--- code/huse/json/Serializer.hpp
+++ code/huse/json/Serializer.hpp
@@ -26,7 +26,7 @@ public:
     // json imposed limits (max integer which can be stored in a double)
     static inline constexpr int64_t Max_Int64 = 9007199254740992ll;
     static inline constexpr int64_t Min_Int64 = -9007199254740992ll;
-    static inline constexpr int64_t Max_Uint64 = 9007199254740992ull;
+    static inline constexpr uint64_t Max_Uint64 = 9007199254740992ull;
 
 private:
     virtual void write(bool val) override;
