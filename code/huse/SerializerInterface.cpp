59b22cd14bde9afa4979111e980299d6d9d06c21
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-03-20 20:18:16 +0200

    Common serializer interface

--- code/huse/SerializerInterface.cpp
+++ code/huse/SerializerInterface.cpp
@@ -3,9 +3,31 @@
 //
 #include "SerializerInterface.hpp"
 #include "DefineMsg.hpp"
+#include "Serializer2.hpp"
 
 namespace huse {
 HUSE_DEFINE_SERIALIZE_MSG(bool, bool);
 HUSE_DEFINE_SERIALIZE_MSG(short, short);
 HUSE_DEFINE_SERIALIZE_MSG(unsigned short, ushort);
+HUSE_DEFINE_SERIALIZE_MSG(int, int);
+HUSE_DEFINE_SERIALIZE_MSG(unsigned int, uint);
+HUSE_DEFINE_SERIALIZE_MSG(long, long);
+HUSE_DEFINE_SERIALIZE_MSG(unsigned long, ulong);
+HUSE_DEFINE_SERIALIZE_MSG(long long, llong);
+HUSE_DEFINE_SERIALIZE_MSG(unsigned long long, ullong);
+HUSE_DEFINE_SERIALIZE_MSG(float, float);
+HUSE_DEFINE_SERIALIZE_MSG(double, double);
+HUSE_DEFINE_SERIALIZE_MSG(std::string_view, sv);
+HUSE_DEFINE_SERIALIZE_MSG(std::nullptr_t, nullptr_t);
+HUSE_DEFINE_SERIALIZE_MSG(std::nullopt_t, nullopt_t);
+
+DYNAMIX_DEFINE_SIMPLE_MSG(openStringStream_msg, unicast);
+DYNAMIX_DEFINE_SIMPLE_MSG(closeStringStream_msg, unicast);
+
+DYNAMIX_DEFINE_SIMPLE_MSG(openObject_msg, unicast);
+DYNAMIX_DEFINE_SIMPLE_MSG(closeObject_msg, unicast);
+DYNAMIX_DEFINE_SIMPLE_MSG(openArray_msg, unicast);
+DYNAMIX_DEFINE_SIMPLE_MSG(closeArray_msg, unicast);
+
+DYNAMIX_DEFINE_SIMPLE_MSG(throwSerializerException_msg, unicast);
 }
--- code/huse/SerializerInterface.hpp
+++ code/huse/SerializerInterface.hpp
@@ -5,10 +5,43 @@
 #include "API.h"
 #include "DeclareMsg.hpp"
 
+#include <string_view>
+#include <optional>
+
 namespace huse {
 HUSE_SERIALIZE_MSG(HUSE_API, bool, bool);
 HUSE_SERIALIZE_MSG(HUSE_API, short, short);
 HUSE_SERIALIZE_MSG(HUSE_API, unsigned short, ushort);
+HUSE_SERIALIZE_MSG(HUSE_API, int, int);
+HUSE_SERIALIZE_MSG(HUSE_API, unsigned int, uint);
+HUSE_SERIALIZE_MSG(HUSE_API, long, long);
+HUSE_SERIALIZE_MSG(HUSE_API, unsigned long, ulong);
+HUSE_SERIALIZE_MSG(HUSE_API, long long, llong);
+HUSE_SERIALIZE_MSG(HUSE_API, unsigned long long, ullong);
+HUSE_SERIALIZE_MSG(HUSE_API, float, float);
+HUSE_SERIALIZE_MSG(HUSE_API, double, double);
+HUSE_SERIALIZE_MSG(HUSE_API, std::string_view, sv);
+
+HUSE_SERIALIZE_MSG(HUSE_API, std::nullptr_t, nullptr_t); // write null explicitly
+HUSE_SERIALIZE_MSG(HUSE_API, std::nullopt_t, nullopt_t); // discard current value
+
+// helper
+void husePolySerialize(Serializer& s, const char* str) { husePolySerialize(s, std::string_view(str)); }
+
+// private interface
+
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, openStringStream_msg, std::ostream&(Serializer&));
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, closeStringStream_msg, void(Serializer&));
+
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, openObject_msg, void(Serializer&));
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, closeObject_msg, void(Serializer&));
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, openArray_msg, void(Serializer&));
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, closeArray_msg, void(Serializer&));
+
+DYNAMIX_DECLARE_EXPORTED_SIMPLE_MSG(HUSE_API, throwSerializerException_msg, void(Serializer&, const std::string&));
+[[noreturn]] void throwSerializerException(Serializer& s, const std::string& msg) {
+    throwSerializerException_msg::call(s, msg);
+}
 }
 
 #define huse_BasicInterface
