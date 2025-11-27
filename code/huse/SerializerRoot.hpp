a5f8f0e542f2eeb5b7997833949fdd2326cd4f43
Borislav Stanimirov <b@ibob.bg>
2025-11-27 11:41:22 +0200

    template serializer root

--- code/huse/SerializerRoot.hpp
+++ code/huse/SerializerRoot.hpp
@@ -6,4 +6,14 @@
 
 namespace huse {
 
+template <typename SSerializer>
+class SerializerRoot : public SSerializer, public SerializerNode {
+public:
+    template <typename... Args>
+    SerializerRoot(Args&&... args)
+        : SSerializer(std::forward<Args>(args)...)
+        , SerializerNode(*this, nullptr)
+    {}
+};
+
 } // namespace huse
--- code/huse/json/SerializerRoot.hpp
+++ code/huse/json/SerializerRoot.hpp
@@ -2,18 +2,11 @@
 // SPDX-License-Identifier: MIT
 //
 #pragma once
-#include "../API.h"
 #include "Serializer.hpp"
-#include "../Serializer.hpp"
+#include "../SerializerRoot.hpp"
 
 namespace huse::json {
 
-class SerializerRoot : public JsonSerializer, public SerializerNode {
-public:
-    SerializerRoot(std::ostream& out, bool pretty = false)
-        : JsonSerializer(out, pretty)
-        , SerializerNode(*this, nullptr)
-    {}
-};
+using SerializerRoot = huse::SerializerRoot<JsonSerializer>;
 
 } // namespace huse::json
