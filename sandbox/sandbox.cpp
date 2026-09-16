715460bd9b5819d6029cf056af63f51ae88500f3
Borislav Stanimirov <b@ibob.bg>
2026-09-16 08:38:21 +0300

    rename scratch to sandbox

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: MIT
 //
 #include <iostream>
-#include <huse/DeserializerNode.hpp>
-#include <huse/json/Serializer.hpp>
 #include <huse/Exception.hpp>
 
 //// raw deserializer value
