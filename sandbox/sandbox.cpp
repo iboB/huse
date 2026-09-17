7a5b5964948c116162b83ffdf77347820485d71e
Borislav Stanimirov <b@ibob.bg>
2026-09-17 07:32:26 +0300

    sandbox: simple exception example

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -2,62 +2,22 @@
 // SPDX-License-Identifier: MIT
 //
 #include <iostream>
-#include <huse/Exception.hpp>
-
-//// raw deserializer value
-//struct ImValue {
-//public:
-//    ImValue()
-//        : m_type(Type::Null)
-//        , m_payload(nullptr)
-//        , m_buffer(nullptr)
-//    {
-//    }
-//
-//    Type type() const { return m_type; }
-//
-//    std::string_view getString() const {
-//        assert(m_type.isString());
-//        const auto ibegin = m_payload[0];
-//        const auto iend = m_payload[1];
-//        return { m_buffer + ibegin, iend - ibegin };
-//    }
-//
-//
-//
-//protected:
-//    ImValue(Type t, const size_t* payload, const std::byte* buffer)
-//        : m_type(t)
-//        , m_payload(payload)
-//        , m_buffer(buffer)
-//    {
-//    }
-//
-//    Type m_type;
-//
-//    // depending on type this containes either the value
-//    // or (for strings and blobs) size and offset in m_buffer
-//    const size_t* m_payload;
-//
-//    // the buffer is used for both strings and blobs
-//    static_assert(sizeof(char) == 1);
-//    const char* m_buffer;
-//};
+#include <huse/json/DeRoot.hpp>
 
 int main()
 {
-    //constexpr std::string_view json = R"({"ar": [2.3, -5], "val": 5, "b": false})";
+    constexpr std::string_view json = R"({"ar": [2.3, -5], "val": 5, "b": false})";
 
-    //try
-    //{
-    //    auto d = huse::json::DeserializerRoot::create(json);
-    //    int i;
-    //    d.val(i);
-    //}
-    //catch (std::exception& e)
-    //{
-    //    std::cout << e.what() << std::endl;
-    //    return 1;
-    //}
+    try
+    {
+        auto d = huse::json::DeRoot(huse::Parse, json);
+        //int i;
+        //d.val(i);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
