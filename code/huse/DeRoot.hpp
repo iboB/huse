9ea65b1af133dd39569a21d9c1dd42ae5b0c658b
Borislav Stanimirov <b@ibob.bg>
2026-09-17 09:18:50 +0300

    de root: work around a gcc vtable ubsan issue

--- code/huse/DeRoot.hpp
+++ code/huse/DeRoot.hpp
@@ -7,16 +7,20 @@
 namespace huse {
 
 template <typename De>
-class DeRoot : public De, public DeNode<De> {
-    De& initRoot() {
-        De::initRoot();
-        return *this;
+class DeRoot final : public De, public DeNode<De> {
+    // this was converted to static function from a class method
+    // because the method led to gcc (erroneously) creating a vtable for DeRoot
+    // which leads to ubsan errors
+    // the static function works around the issue
+    static De& initRoot(De& d) {
+        d.initRoot();
+        return d;
     }
 public:
     template <typename... Args>
     DeRoot(Args&&... args)
         : De(std::forward<Args>(args)...)
-        , DeNode<De>(initRoot(), true)
+        , DeNode<De>(initRoot(*this), true)
     {}
 
     // copy and move can be implemented (maintaining self-reference), but it's not needed at this time
